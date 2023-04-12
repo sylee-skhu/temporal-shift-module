6d6b0ba9b2dfe660cd26fbd3be22f9f00d810ebe
Sangyun Lee <sylee@skhu.ac.kr>
2023-04-12 20:15:28 +0900

    add exp config

--- opts.py
+++ opts.py
@@ -4,7 +4,8 @@
 # {jilin, songhan}@mit.edu, ganchuang@csail.mit.edu
 
 import argparse
-parser = argparse.ArgumentParser(description="PyTorch implementation of Temporal Segment Networks")
+parser = argparse.ArgumentParser(
+    description="PyTorch implementation of Temporal Segment Networks")
 parser.add_argument('dataset', type=str)
 parser.add_argument('modality', type=str, choices=['RGB', 'Flow'])
 parser.add_argument('--train_list', type=str, default="")
@@ -20,10 +21,12 @@ parser.add_argument('--dropout', '--do', default=0.5, type=float,
                     metavar='DO', help='dropout ratio (default: 0.5)')
 parser.add_argument('--loss_type', type=str, default="nll",
                     choices=['nll'])
-parser.add_argument('--img_feature_dim', default=256, type=int, help="the feature dimension for each frame")
+parser.add_argument('--img_feature_dim', default=256, type=int,
+                    help="the feature dimension for each frame")
 parser.add_argument('--suffix', type=str, default=None)
 parser.add_argument('--pretrain', type=str, default='imagenet')
-parser.add_argument('--tune_from', type=str, default=None, help='fine-tune from checkpoint')
+parser.add_argument('--tune_from', type=str, default=None,
+                    help='fine-tune from checkpoint')
 
 # ========================= Learning Configs ==========================
 parser.add_argument('--epochs', default=120, type=int, metavar='N',
@@ -42,7 +45,8 @@ parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
                     metavar='W', help='weight decay (default: 5e-4)')
 parser.add_argument('--clip-gradient', '--gd', default=None, type=float,
                     metavar='W', help='gradient norm clipping (default: disabled)')
-parser.add_argument('--no_partialbn', '--npb', default=False, action="store_true")
+parser.add_argument('--no_partialbn', '--npb',
+                    default=False, action="store_true")
 
 # ========================= Monitor Configs ==========================
 parser.add_argument('--print-freq', '-p', default=20, type=int,
@@ -63,14 +67,22 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('--gpus', nargs='+', type=int, default=None)
 parser.add_argument('--flow_prefix', default="", type=str)
-parser.add_argument('--root_log',type=str, default='log')
+parser.add_argument('--root_log', type=str, default='log')
 parser.add_argument('--root_model', type=str, default='checkpoint')
 
-parser.add_argument('--shift', default=False, action="store_true", help='use shift for models')
-parser.add_argument('--shift_div', default=8, type=int, help='number of div for shift (default: 8)')
-parser.add_argument('--shift_place', default='blockres', type=str, help='place for shift (default: stageres)')
+parser.add_argument('--shift', default=False,
+                    action="store_true", help='use shift for models')
+parser.add_argument('--shift_div', default=8, type=int,
+                    help='number of div for shift (default: 8)')
+parser.add_argument('--shift_place', default='blockres',
+                    type=str, help='place for shift (default: stageres)')
 
-parser.add_argument('--temporal_pool', default=False, action="store_true", help='add temporal pooling')
-parser.add_argument('--non_local', default=False, action="store_true", help='add non local block')
+parser.add_argument('--temporal_pool', default=False,
+                    action="store_true", help='add temporal pooling')
+parser.add_argument('--non_local', default=False,
+                    action="store_true", help='add non local block')
 
-parser.add_argument('--dense_sample', default=False, action="store_true", help='use dense sample for video dataset')
+parser.add_argument('--dense_sample', default=False,
+                    action="store_true", help='use dense sample for video dataset')
+
+parser.add_argument('--exp', default=None, type=str)
