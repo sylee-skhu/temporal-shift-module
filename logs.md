5d7488b936aa901bceb24c2e07948ae78145fc54
Sangyun Lee <sylee@skhu.ac.kr>
2023-04-12 20:22:14 +0900

    update logs.md

--- logs.md
+++ logs.md
@@ -12,9 +12,9 @@ Overall Prec@1 61.68% Prec@5 87.38%
 ## base-minus
 
 ```
-python3 main.py somethingv2 RGB --arch resnet50 --num_segments 8 --gd 20 --lr 0.01 --lr_steps 20 40 --epochs 50 --batch-size 64 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 --shift --shift_div=8 --shift_place=blockres --npb
+python3 main.py somethingv2 RGB --arch resnet50 --num_segments 8 --gd 20 --lr 0.01 --lr_steps 20 40 --epochs 50 --batch-size 64 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 --shift --shift_div=8 --shift_place=blockres --npb --exp=minus
 
-python3 test_models.py somethingv2 --weights=checkpoint/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e50/ckpt.best.pth.tar --test_segments=8 --batch_size=72 -j 24 --test_crops=3 --twice_sample
+python3 test_models.py somethingv2 --weights=checkpoint/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e50/ckpt.best.pth.tar --test_segments=8 --batch_size=72 -j 24 --test_crops=3 --twice_sample --exp=minus
 ```
 
 Class Accuracy 55.34%
--- ops/temporal_shift.py
+++ ops/temporal_shift.py
@@ -24,11 +24,11 @@ class TemporalShift(nn.Module):
 
     def forward(self, x):
         x = self.shift(x, self.n_segment, fold_div=self.fold_div,
-                       inplace=self.inplace)
+                       inplace=self.inplace, exp=self.exp)
         return self.net(x)
 
     @staticmethod
-    def shift(x, n_segment, fold_div=3, inplace=False):
+    def shift(x, n_segment, fold_div=3, inplace=False, exp=None):
         nt, c, h, w = x.size()
         n_batch = nt // n_segment
         x = x.view(n_batch, n_segment, c, h, w)
@@ -39,18 +39,18 @@ class TemporalShift(nn.Module):
             # May need to write a CUDA kernel.
             raise NotImplementedError
             # out = InplaceShift.apply(x, fold)
-        else:  # 16f: 21073920 = 0.021G 8f: 9834496 = 0.010G
+        elif exp is "minus":
             out = torch.zeros_like(x)
             merged = torch.sub(x[:, :-1, :2 * fold], x[:, 1:, :2 * fold])
             out[:, :-1, :fold] = merged[:, :, :fold]
             out[:, 1:, fold: 2 * fold] = merged[:, :, fold: 2 * fold]
             out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # not shift
-        # else:
-        #     out = torch.zeros_like(x)
-        #     out[:, :-1, :fold] = x[:, 1:, :fold]  # shift left
-        #     out[:, 1:, fold: 2 * fold] = x[:, :-
-        #                                    1, fold: 2 * fold]  # shift right
-        #     out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # not shift
+        else:
+            out = torch.zeros_like(x)
+            out[:, :-1, :fold] = x[:, 1:, :fold]  # shift left
+            out[:, 1:, fold: 2 * fold] = x[:, :-
+                                           1, fold: 2 * fold]  # shift right
+            out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # not shift
 
         return out.view(nt, c, h, w)
 
