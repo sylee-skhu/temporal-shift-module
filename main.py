04c33ec5d2ce225d4ebbb73aa46343139b438564
Sangyun Lee <sylee@skhu.ac.kr>
2023-04-13 16:25:15 +0900

    bug fix

--- main.py
+++ main.py
@@ -63,7 +63,8 @@ def main():
                 is_shift=args.shift, shift_div=args.shift_div, shift_place=args.shift_place,
                 fc_lr5=not (args.tune_from and args.dataset in args.tune_from),
                 temporal_pool=args.temporal_pool,
-                non_local=args.non_local)
+                non_local=args.non_local,
+                exp=args.exp)
 
     crop_size = model.crop_size
     scale_size = model.scale_size
